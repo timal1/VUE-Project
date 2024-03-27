8f48c59d950113755432d67b673c81889f850e26
tladatko.adm <tladatko@sitronics.com>
2024-03-27 11:02:10 +0300

    add VueX

---
+++ src/components/FirstCard.vue
@@ -0,0 +1,139 @@
+<template>
+  <article class="projects__card">
+    <figure class="projects__card-figure" :class="card.class">
+      <img class="projects__card-img" :src="card.imgsrc" :alt="imgalt" />
+    </figure>
+    <div class="projects__text-card">
+      <div>
+        <h4 class="projects__card-headding" v-html="card.title"></h4>
+        <TextComp>{{ card.info }}</TextComp>
+      </div>
+      <router-link to="/project/details">
+        <ArrowBtn
+          :btnclass="button.class"
+          :btnwidth="button.width"
+          :btnheight="button.height"
+        ></ArrowBtn>
+      </router-link>
+    </div>
+  </article>
+</template>
+
+<script>
+import TextComp from "./TextComp.vue";
+import ArrowBtn from "./ArrowBtn.vue";
+
+export default {
+  components: { TextComp, ArrowBtn },
+  props: {
+    card: Object,
+  },
+  data() {
+    return {
+      button: {
+        height: 20.039,
+        width: 10.02,
+        class: "projects__btn",
+      },
+      // cards: [
+      //   {
+      //     imgsrc: require("@/assets/img/card1.png"),
+      //     link: "#",
+      //     imgalt: "Modern Kitchan",
+      //     title: "Modern Kitchan",
+      //     info: "Decor / Artchitecture",
+      //     class: "btrr",
+      //   },
+      //   {
+      //     imgsrc: require("@/assets/img/card2.png"),
+      //     link: "#",
+      //     imgalt: "Modern Kitchan",
+      //     title: "Modern Kitchan",
+      //     info: "Decor / Artchitecture",
+      //     class: "btlr",
+      //   },
+      //   {
+      //     imgsrc: require("@/assets/img/card3.png"),
+      //     link: "#",
+      //     imgalt: "Modern Kitchan",
+      //     title: "Modern Kitchan",
+      //     info: "Decor / Artchitecture",
+      //     class: "bbrr",
+      //   },
+      //   {
+      //     imgsrc: require("@/assets/img/card4.png"),
+      //     link: "#",
+      //     imgalt: "Modern Kitchan",
+      //     title: "Modern Kitchan",
+      //     info: "Decor / Artchitecture",
+      //     class: "bblr",
+      //   },
+      // ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main-figure {
+    height: 525px;
+}
+.projects {
+  &__card {
+    width: 45.8%;
+  }
+  .btrr {
+    border-top-right-radius: 80px;
+  }
+  .btlr {
+    border-top-left-radius: 80px;
+  }
+  .bbrr {
+    border-bottom-right-radius: 80px;
+  }
+  .bblr {
+    border-bottom-left-radius: 80px;
+  }
+
+  &__text-card {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 24px;
+  }
+  &__card-headding {
+    color: rgb(41, 47, 54);
+    font-size: 25px;
+    font-weight: 400;
+    line-height: 125%;
+  }
+  &__info {
+    padding-top: 5px;
+  }
+  &__btn {
+    display: flex;
+    background: rgb(244, 240, 236);
+    text-align: center;
+    border-radius: 100%;
+    padding: 25px 30px;
+    transition: all 0.5s;
+  }
+
+  &__btn:hover {
+    cursor: pointer;
+    background-color: #fff;
+  }
+
+  // .articles__btn {
+  //   background: rgb(244, 240, 236);
+  //   text-align: center;
+  //   border-radius: 100%;
+  //   padding: 18px 22px;
+  //   transition: all 0.5s;
+  // }
+  // .articles__btn:hover {
+  //   cursor: pointer;
+  //   background-color: #fff;
+  // }
+}
+</style>
---
+++ src/components/SwipperZoom.vue
@@ -0,0 +1,81 @@
+<template>
+  <swiper
+    :style="{
+      '--swiper-navigation-color': '#fff',
+      '--swiper-pagination-color': '#000',
+    }"
+    :zoom="true"
+    :navigation="false"
+    :pagination="{
+      clickable: true,
+    }"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="@/assets/img/prog-img1.png" />
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="@/assets/img/card1.png" />
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="@/assets/img/card2.png" />
+      </div>
+    </swiper-slide>
+  </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/zoom";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Zoom, Navigation, Pagination } from "swiper/modules";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Zoom, Navigation, Pagination],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  position: relative;
+  height: auto;
+  width: 1200px;
+  padding-top: 70%;
+  border-radius: 70px;
+  background: rgb(196, 196, 196);
+  overflow: hidden;
+}
+
+.swiper-zoom-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+.swiper-zoom-container > img {
+  max-height: initial;
+  width: 100%;
+}
+</style>
